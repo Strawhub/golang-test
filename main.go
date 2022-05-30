cf294b35a85661b8a9ae1597f65ec944e4b8fccc
Straw <k7to.so23.ten717@i.softbank.jp>
2022-05-30 15:04:57 +0900

    変数3つに変更

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "test/add"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
+	"test/add"
 )
 
 func main() {
-    a, _ := strconv.Atoi(os.Args[1])
-    b, _ := strconv.Atoi(os.Args[2])
-    fmt.Println(add.Add(a, b))
+	a, _ := strconv.Atoi(os.Args[1])
+	b, _ := strconv.Atoi(os.Args[2])
+	c, _ := strconv.Atoi(os.Args[2])
+	fmt.Println(add.Add(a, b, c))
 }
